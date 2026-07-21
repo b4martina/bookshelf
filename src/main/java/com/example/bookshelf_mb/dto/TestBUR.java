688123bda70c9ea581adf47b07fa063d73bcf485
b4martina <martinableta13@gmail.com>
2026-07-21 02:25:05 +0200

    new changes made to the project. added a way to get all read books by all users and read books of only one user.

---
+++ src/main/java/com/example/bookshelf_mb/dto/TestBUR.java
@@ -0,0 +1,84 @@
+package com.example.bookshelf_mb.dto;
+
+import com.example.bookshelf_mb.model.BookStatus;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({
+        "id",
+        "title",
+        "author",
+        "timesRead",
+        "status",
+        "name",
+        "surname"
+})
+public class TestBUR {
+    private Long id;
+    private String title;
+    private String author;
+    private BookStatus status;
+    private int timesRead;
+    private String name;
+    private String surname;
+
+
+    public TestBUR(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
+
+    public int getTimesRead() {
+        return timesRead;
+    }
+
+    public void setTimesRead(int timesRead) {
+        this.timesRead = timesRead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
